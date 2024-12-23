8e3432f8f3c968e571db70809f258a2c18850564
shivrajan sharma <shivranjankumar917@gmail.com>
2024-12-23 10:18:14 +0530

    day 3 learn basic of sys module

---
+++ basic_python.py
@@ -0,0 +1,60 @@
+#learnin sys module in python
+import sys
+
+
+
+
+
+# print("the version of the machine is " , sys.version)
+# print(sys.argv)
+# print(sys.platform)
+# sys.exit(3)
+
+
+# print("script name", sys.argv[0])
+# print("number of arguments", sys.argv[2:])
+
+
+# sys.stdout.write("this is a standard output\n")
+# sys.stderr.write("this is a standard error\n")
+
+
+
+# try:
+#     x = 1/0
+# except ZeroDivisionError:
+#     print("division by zero")
+#     sys.exit(1)
+
+# Memory Management
+
+
+# x = [10,20,30,40,50]
+# print(sys.getsizeof(x))
+
+
+# print(sys.getrecursionlimit())
+# a = sys.setrecursionlimit(2000)
+# print(a)
+
+
+#execution information 
+
+# try:
+#     x = 1 / 0
+# except ZeroDivisionError:
+#     print(sys.exc_info())
+
+
+
+
+
+#byte order 
+
+
+print(sys.byteorder)
+
+
+
+
+
